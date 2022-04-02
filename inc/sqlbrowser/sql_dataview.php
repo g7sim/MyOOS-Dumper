420418bfc5d30f4b890fd85e012a11c09ddb2c06
r23 <info@r23.de>
2022-04-02 18:16:38 +0200

    allow non-lowercase tablenames Thx 2 squigulum https://github.com/squigulum/MySQLDumper/commit/77235f52098715f0e5c73571cab2df5cc9051d89

--- inc/sqlbrowser/sql_dataview.php
+++ inc/sqlbrowser/sql_dataview.php
@@ -106,10 +106,11 @@ if (0 == $sql_to_display_data) {
 
     if ('select ' == substr($sql_temp, 0, 7)) {
         if (false !== strpos($sql_temp, ' limit ')) {
-            // es wurde ein eigenes Lmit im Query angegeben - eigene Berechnung abbrechen
+            // es wurde ein eigenes Limit im Query angegeben - eigene Berechnung abbrechen
             $numrowsabs = -1;
         } else {
-            $sql_temp = 'SELECT count(*) as anzahl FROM ('.$sql_temp.') as query;';
+			// anstatt sql_temp in lowerase hier das 'original' sql_statement verwenden
+			$sql_temp = "SELECT count(*) as anzahl FROM (".$sql['sql_statement'].") as query;";
             $res = @mod_query($sql_temp, false);
             if ($res) {
                 if ($row = mysqli_fetch_object($res)) {
